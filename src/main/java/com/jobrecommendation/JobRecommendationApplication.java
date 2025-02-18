e21aa7087544177143a2fc8b23cfe6d9905979a0
Shreyas C.S. <csshreyas28@gmail.com>
2025-02-18 11:11:05 +0530

    Disable Spring Security for '/api/hello' endpoint

--- src/main/java/com/jobrecommendation/JobRecommendationApplication.java
+++ src/main/java/com/jobrecommendation/JobRecommendationApplication.java
@@ -1,12 +1,22 @@
 package com.jobrecommendation;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+public class JobRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRecommendationApplication.class, args);
+    }
+}
+
 @RestController
 @RequestMapping("/api")
-public class DemoController {
+class DemoController {
 
     @GetMapping("/hello")
     public String sayHello() {
---
+++ src/main/java/com/jobrecommendation/WebSecurityConfigurerAdapter.java
@@ -0,0 +1,5 @@
+package com.jobrecommendation;
+
+public class WebSecurityConfigurerAdapter {
+
+}
