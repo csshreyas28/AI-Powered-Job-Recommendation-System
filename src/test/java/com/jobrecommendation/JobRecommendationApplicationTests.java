ddd3744f78485a5211327ee982e801aa3c00aaf4
Shreyas C.S. <csshreyas28@gmail.com>
2025-02-18 10:51:45 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/jobrecommendation/JobRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
